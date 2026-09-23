901774e95d2423e33213d2f3848a707d2df35bd2
osmany-perez-1998 <58227676+osmany-perez-1998@users.noreply.github.com>
2026-09-24 00:46:55 +0100

    Add: longest-repeating-substring-with-replacement - submission-21

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-21.py
@@ -0,0 +1,40 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int: 
+
+        max_freq_letter= ""
+        freq_dic = {}
+
+        max_length = 0
+        sliding_window = 0
+
+
+        for i in range (len(s)):
+
+            if freq_dic.get(s[i],0):
+                freq_dic [s[i]] +=1
+            else:
+                freq_dic[s[i]] = 1
+
+            if not k:
+                if i - sliding_window + 1 == freq_dic[s[i]]:
+                    max_length = max(max_length,freq_dic[s[i]])
+                    
+            if freq_dic[s[i]] >= freq_dic.get(max_freq_letter,0):
+                max_freq_letter = s[i]
+
+            possible_string_balance = i - sliding_window + 1  - k  - freq_dic[max_freq_letter]
+            if possible_string_balance <= 0:
+                max_length = max(max_length, i - sliding_window + 1 )
+            elif i - sliding_window > max_length:
+                freq_dic[s[sliding_window]] -= 1
+                sliding_window+=1
+                possible_string_balance = i - sliding_window + 1  - k  - freq_dic[max_freq_letter]
+                if possible_string_balance <= 0:
+                    max_length = max(max_length, i - sliding_window + 1 )
+                
+
+
+            
+
+        return max_length
+        
